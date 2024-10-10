cce63039d9ec55d28a14495b7d6f15fb686e7117
victor <zhanghaifeng903@sina.com>
2024-10-10 11:05:46 +0800

    remove old code

--- library/src/main/java/com/victor/zxing/library/data/Result.java
+++
@@ -1,9 +0,0 @@
-package com.victor.zxing.library.data;
-
-import java.io.Serializable;
-
-/**
- * Created by hupei on 2016/8/12.
- */
-class Result implements Serializable {
-}
--- library/src/main/java/com/victor/zxing/library/interfaces/OnQrScanListener.java
+++
@@ -1,9 +0,0 @@
-package com.victor.zxing.library.interfaces;
-
-/**
- * Created by Administrator on 2017/11/20.
- */
-
-public interface OnQrScanListener {
-    void OnQrScan (String resultString);
-}
